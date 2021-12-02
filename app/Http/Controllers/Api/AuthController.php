e360ba7c834b4faf18f4016514e79f7aaa65fdaf
krismandev <krismanpratama@gmail.com>
2021-12-03 03:35:41 +0700

    upload

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -77,7 +77,7 @@ class AuthController extends Controller
             $user->name = $request->name;
             $user->lastname = $request->lastname;
             $photo = '';
-            return response()->json($request->photo);
+            dd($request->photo);
             if ($request->photo != '') {
                 $photo = time().'.jpg';
                 //decode photo string and save to storage/profiles
